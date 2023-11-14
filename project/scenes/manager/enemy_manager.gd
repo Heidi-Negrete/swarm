b3e00c57c35d30ab01651aa928257bc81461d479
Heidi Negrete <heidi@negrete.dev>
2023-11-13 19:51:07 -0500

    adjust enemy spawn

--- project/scenes/manager/enemy_manager.gd
+++ project/scenes/manager/enemy_manager.gd
@@ -17,17 +17,35 @@ func _on_timer_timeout():
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
 		return
-	
-	
-	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
-	var spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+
 	var enemy = basic_enemy_scene.instantiate() as Node2D
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	entities_layer.add_child(enemy)
-	enemy.global_position = spawn_position
+	enemy.global_position = get_spawn_position()
 
 
 func _on_arena_time_manager_arena_difficulty_increased(arena_difficulty):
 	var time_off = (.1 / 12) * arena_difficulty
 	time_off = min(time_off, .7)
 	$Timer.wait_time = base_spawn_time - time_off
+
+
+func get_spawn_position():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	
+	if player == null:
+		return Vector2.ZERO
+		
+	var spawn_position = Vector2.ZERO
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	for i in 4:
+		spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+		var query_parameters = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, 1)
+		var result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_parameters)
+
+		if result.is_empty():
+			break
+		else:
+			random_direction = random_direction.rotated(.25 * TAU)
+	
+	return spawn_position
